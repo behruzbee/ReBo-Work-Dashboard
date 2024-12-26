466d123a2b08627f5034a9668773457946cd3840
Behruz <behruz@bictory.io>
2024-12-26 17:43:37 +0500

    fixed [21]

--- src/pages/layouts/ui.tsx
+++ src/pages/layouts/ui.tsx
@@ -7,7 +7,7 @@ import s from './styles.module.scss';
 import { Sidebar } from './components/sidebar';
 
 const LINKS = [
-  { path: RouterPaths.workers.root, icon: 'male', text: "Ishchilar", permissionLevel: basePermissions.worker.readWorkers },
+  { path: RouterPaths.workers.root, icon: 'male', text: "Xodimlar", permissionLevel: basePermissions.worker.readWorkers },
   { path: RouterPaths.histories.root, icon: 'history', text: "Tarix", permissionLevel: basePermissions.history.readHistories },
   { path: RouterPaths.penalties.root, icon: 'money-withdraw ', text: "Jarimalar", permissionLevel: basePermissions.penalty.read },
   { path: RouterPaths.bonuses.root, icon: 'money', text: "Bonuslar", permissionLevel: basePermissions.bonus.read },
